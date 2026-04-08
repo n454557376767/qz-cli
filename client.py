5f000582fb6a1fa763f120d8d87519d8cf71e1d4
nm123 <16166118+nm123_7878@user.noreply.gitee.com>
2026-04-08 11:50:04 +0800

    feat:新增发送帖子

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import httpx
-
+import json
 def login(username,password):
     with httpx.Client(base_url="https://hehenya.dpdns.org:8505") as client:
         headers = {"Content-Type":"application/json"}
@@ -10,3 +10,70 @@ def login(username,password):
         response_login = client.post('/login',json=json_data)
         return response_login.json()
         
+        
+# 此处来自qz_user_sdk        
+class Post:
+    def __init__(self,token = ""):
+        self.base_url = "https://hehenya.dpdns.org:8505"
+        self.token = token        
+        self.headers = {
+            "Accept-Language": "zh-cn,zh;q=0.5",
+            "Accept-Charset": "UTF-8",
+            "x-access-token": self.token,
+            "Content-Type": "application/json",
+            "Connection": "Keep-Alive",
+            "Accept-Encoding": "gzip"
+        }
+
+    def send_message(self, content, category_id, message_type=None, is_markdown=None,title=None):
+        url = f"{self.base_url}/post_message"
+        data = {
+            "category_id": category_id,
+            "content": content
+        }
+        
+        if is_markdown:
+            data["is_markdown"] = is_markdown
+        if message_type:
+            data["message_type"] = message_type
+        if title:
+            data["title"] = title    
+        try:
+            response = httpx.post(url, headers=self.headers, data=json.dumps(data))
+            return response.json()
+        except Exception as e:
+            return f"请求出错: {str(e)}"
+
+    def like_message(self, message_id):
+        url = f"{self.base_url}/like_message"
+        data = {"message_id": message_id}
+        
+        try:
+            response = httpx.post(url, headers=self.headers, data=json.dumps(data))
+            return response.json()
+        except Exception as e:
+            return str(e)
+
+    def get_messages(self, category_id, page, per_page):
+        url = f"{self.base_url}/v3/get_message?category_id={category_id}&page={page}&per_page={per_page}"
+        
+        try:
+            response = httpx.get(url, headers=self.headers)
+            return response.json()
+        except httpx.RequestException as e:
+            return f"请求出错: {str(e)}"
+            
+    def reply_to_message(self, content, category_id, referenced_message_id):
+        url = f"{self.base_url}/post_referenced_message"
+        data = {
+            "content": content,
+            "category_id": category_id,
+            "referenced_message_id": referenced_message_id
+        }
+        
+        try:
+            response = httpx.post(url, headers=self.headers, data=json.dumps(data))
+            return response.json()
+        except Exception as e:
+            return f"请求出错: {str(e)}"
+            
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import click
 import client
 from dotenv import dotenv_values, set_key
+from dotenv import load_dotenv
+import os
+import readline
+load_dotenv()
+qz_post = client.Post(token = os.getenv('token'))
 
 @click.group()
 def cli():
@@ -19,6 +24,36 @@ def login(username,password):
         return
     set_key('.env',"token",token)
     click.echo("已写入token")
+@cli.group()
+def post():
+    """帖子功能"""
+    pass
     
+@post.command()
+@click.option("-c","--content",help="帖子内容")
+@click.option("-t","--title",default=None,help="帖子标题")
+@click.option("-m","--is_markdown",is_flag=True,help="是否为markdown")
+@click.option("--category_id",help="分区id")
+@click.option("--message_type",default=None,help="帖子类型")
+@click.option("--debug",default=False,is_flag=True,help="Debug模式")
+def send_post(content, title, is_markdown, category_id, message_type, debug):
+    if is_markdown:
+        response = qz_post.send_message(
+            content=content,
+            title=title,
+            message_type=message_type,
+            category_id=category_id,
+            is_markdown=True
+        )
+    else:
+        response = qz_post.send_message(
+            content=content,
+            title=title,
+            message_type=message_type,
+            category_id=category_id
+        )
+    if debug:
+        click.echo(f"API返回：{response}")
+    click.echo(f"已成功尝试发送")
 if __name__ == "__main__":
     cli()
